eac9f9d77cc3b9bd705b9130e1dbd428812cc773
Israel Akinwumi <israelakin14@gmail.com>
2022-04-15 20:52:03 +0800

    Implemented Dynamic overview page and functional Api page

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const fs = require('fs');
 const http = require('http')
 ////////////////////////////////////////////////////////////
 
-const replaceTemplate = (temp, product) => {
-
-}
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data); /// makes and object
+const dataObj = JSON.parse(data); /// converting data to object
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 
-const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%ID%}/g, product.id)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    }
+    return output
+}
+
+const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+const overviewCompiled = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml)
+
 ////////////////////////////////////////////////////////////
 // SERVER
 
@@ -25,7 +39,7 @@ const server = http.createServer((req, res) => {
     // Overview Page
     if (pathName.toLowerCase() === '/' || pathName.toLowerCase() === '/overview') {
         res.writeHead(202, { 'content-type': 'text/html' })
-        res.end(tempOverview)
+        res.end(overviewCompiled)
     }
 
     // Product Page
